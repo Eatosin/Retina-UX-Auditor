f00aa691e309db8214b7ddbb3965c6a6eaf77cbd
Owadokun Tosin Tobi <89821134+Eatosin@users.noreply.github.com>
2026-01-19 03:30:23 +0100

    Remove ** from logger message

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ logger = logging.getLogger("Retina")
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 7860))
-    logger.info(f"👁️ Retina System Starting on Port {port}...")
+    logger.info(f"Retina System Starting on Port {port}...")
     uvicorn.run("src.main:app", host="0.0.0.0", port=port, reload=False)
